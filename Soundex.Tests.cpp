939ffacae65eee5b96d59e4ce8e1175659af1c0e
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-07-07 02:03:52 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,27 +10,28 @@ TEST(SoundexTest, HandlesSingleCharacter) {
 }
 
 TEST(SoundexTest, HandlesSimpleCases) {
-    EXPECT_EQ(generateSoundex("Robert"), "R130");
-    EXPECT_EQ(generateSoundex("Rupert"), "R130");
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
     EXPECT_EQ(generateSoundex("Rubin"), "R150");
-    EXPECT_EQ(generateSoundex("Ashcraft"), "A213");
-    EXPECT_EQ(generateSoundex("Ashcroft"), "A213");
-    EXPECT_EQ(generateSoundex("Tymczak"), "T200");
-    EXPECT_EQ(generateSoundex("Pfister"), "P230");
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T520");
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
 }
 
 TEST(SoundexTest, HandlesNameWithDuplicates) {
-    EXPECT_EQ(generateSoundex("Aardvark"), "A120");
-    EXPECT_EQ(generateSoundex("Bookkeeper"), "B210");
+    EXPECT_EQ(generateSoundex("Aardvark"), "A631");
+    EXPECT_EQ(generateSoundex("Bookkeeper"), "B216");
 }
 
 TEST(SoundexTest, HandlesNameWithAllVowels) {
     EXPECT_EQ(generateSoundex("Aeiou"), "A000");
+    EXPECT_EQ(generateSoundex("AeIouyHw"), "A000"); 
 }
 
 TEST(SoundexTest, HandlesNameWithMixedCases) {
-    EXPECT_EQ(generateSoundex("roBErt"), "R130");
-    EXPECT_EQ(generateSoundex("RuPErt"), "R130");
+    EXPECT_EQ(generateSoundex("roBErt"), "R163");
+    EXPECT_EQ(generateSoundex("RuPErt"), "R163");
 }
 
 TEST(SoundexTest, HandlesNamesThatMapToSameCode) {
@@ -42,5 +43,5 @@ TEST(SoundexTest, HandlesNameThatRequiresPadding) {
     EXPECT_EQ(generateSoundex("A"), "A000");
     EXPECT_EQ(generateSoundex("R"), "R000");
     EXPECT_EQ(generateSoundex("Ra"), "R000");
-    EXPECT_EQ(generateSoundex("Ray"), "R200");
+    EXPECT_EQ(generateSoundex("Ray"), "R000");
 }
