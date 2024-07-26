17b3935c827a9da0add0eb3df747a60a510818ce
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-07-26 21:10:48 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -4,7 +4,7 @@
 
 std::string generateSoundex(const std::string& name)
 {
-    std::string soundex(1, toupper(name[0]));
+    std::string soundexCode(1, toupper(name[0]));
 
     int matchCount = 1;
     int codeCount = 1;
